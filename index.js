6ca553062e6f4e33838d2312afe633c9c0b87e6b
adxyyz <158788696+adxyyz@users.noreply.github.com>
2024-02-04 02:42:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Europe/Madrid', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -23,19 +23,19 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('1125185075319951521')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
-    .setDetails(`Valorant [${formatTime()}]`)
+    .setURL('https://twitch.tv/adxyzz') //Must be a youtube video link 
+    .setState('Gym 24/7')
+    .setName('adxyy')
+    .setDetails(`dead [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/705742852402970686/1203489898720399430/0c904f520b159d4e6ee89fa52d705079.jpg?ex=65d1484f&is=65bed34f&hm=bb9ebb01229aa94fb3fdb4aae84ce913cb96dd7a2eba0730b5fc7c4e5bc55889&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/705742852402970686/1203496216214642709/c131c5fee6b68f3ea06331381cd50fe6.jpg?ex=65d14e31&is=65bed931&hm=1b13ff3830c81f81e1726a8eac0132f9f7c6b689804f973670f34c81b167ae3e&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Tomorrow...') //Text when you hover the Small image
+    .addButton('Watch', 'https://twitch.tv/adxyzz')
+    .addButton('Donate', 'https://twitch.tv/adxyzz');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `dead[${newTime}]`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
