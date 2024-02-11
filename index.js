4288b4e5ae13d94b5627559591ec0241d70abe4a
adxyyz <158788696+adxyyz@users.noreply.github.com>
2024-02-11 22:25:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ client.on('ready', async () => {
     .setApplicationId('1125185075319951521')
     .setType('STREAMING')
     .setURL('https://twitch.tv/adxyzz') //Must be a youtube video link 
-    .setState('Gym 24/7')
+    .setState('DMs open!')
     .setName('adxyy')
-    .setDetails(`dead [${formatTime()}]`)
+    .setDetails(`Loyalty! [${formatTime()}]`)
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/705742852402970686/1203489898720399430/0c904f520b159d4e6ee89fa52d705079.jpg?ex=65d1484f&is=65bed34f&hm=bb9ebb01229aa94fb3fdb4aae84ce913cb96dd7a2eba0730b5fc7c4e5bc55889&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
+    .setAssetsLargeText('彼女はここにいない...') //Text when you hover the Large image
     .setAssetsSmallImage('https://cdn.discordapp.com/attachments/705742852402970686/1203496216214642709/c131c5fee6b68f3ea06331381cd50fe6.jpg?ex=65d14e31&is=65bed931&hm=1b13ff3830c81f81e1726a8eac0132f9f7c6b689804f973670f34c81b167ae3e&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Tomorrow...') //Text when you hover the Small image
-    .addButton('Watch me', 'https://twitch.tv/adxyzz')
-    .addButton('Pls follow me', 'https://twitch.tv/adxyzz');
+    .addButton('Twich!', 'https://twitch.tv/adxyzz')
+    .addButton('Spotify!', 'https://open.spotify.com/user/qjjkf3wdib3agk1jv7nq5hgnr');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "offline" }); //dnd, online, idle, offline
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `dead[${newTime}]`;
+      const newDetails = `Loyalty![${newTime}]`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
